0d92eb86dd5391f0e617ea57d6780bb9ed4b0684
MREiW <kajbundit_miw@hotmail.com>
2022-06-15 17:46:08 +0700

    สามารถเพิ่มรูปในหน้า profile ได้แล้ว แต่ยัง upload ขึ้น firebase ไม่ได้ Note : Class ที่จะใช้ upload ขึ้น firebase ชื่อว่า upLoadPic

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -1,10 +1,14 @@
 import 'dart:ffi';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:smart_iot_app/pages/MangePage.dart';
 import 'package:smart_iot_app/pages/ProfilePage.dart';
 import 'dart:async';
@@ -12,17 +16,19 @@ import 'dart:convert';
 import 'package:smart_iot_app/services/authentication.dart';
 import 'package:smart_iot_app/services/database_op.dart';
 import 'package:firebase_core/firebase_core.dart';
+//import 'package:path/path.dart';
 
 class Profile_Page extends StatefulWidget {
   const Profile_Page({Key? key, required this.auth}) : super(key: key);
   final BaseAuth auth;
+
   @override
   State<Profile_Page> createState() => _Profile_PageState();
+
 }
 
 class _Profile_PageState extends State<Profile_Page> {
-
-   //UserModel ?currentUser ;
+   File? image;
 
 
   @override
@@ -49,6 +55,14 @@ class _Profile_PageState extends State<Profile_Page> {
       });
     });
   }
+
+
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -96,8 +110,10 @@ class _Profile_PageState extends State<Profile_Page> {
             children: <Widget>[
 
               showProfile(),
+              AddPhoto(),
               TextUser(),
               Email(),
+              SubmitButton(),
               Forgotpassword(),
               SignoutButton(),
 
@@ -109,31 +125,51 @@ class _Profile_PageState extends State<Profile_Page> {
   }
 
   Widget showProfile() {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 5,horizontal: 40),
-      width: 234,
-      height: 280,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(154.5),
-          topRight: Radius.circular(154.5),
-          bottomLeft: Radius.circular(154.5),
-          bottomRight: Radius.circular(154.5),
+    return Builder(
+      builder: (context)=>Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child:  CircleAvatar(
+                    radius: 130,
+                    backgroundColor: Colors.grey,
+                    child: ClipOval(
+                      child: SizedBox(
+                        width: 240,
+                        height: 240,
+                        child: image != null ? Image.file(image!,fit: BoxFit.fill,) : Image.network("https://icon-library.com/images/9272.png",fit:BoxFit.fill ,) ,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.pink.withOpacity(0.2),
-            spreadRadius: 4,
-            blurRadius: 10,
-            offset: Offset(0, 3),
-          )
-        ],
-        image: DecorationImage(
-            image: AssetImage('assets/images/profile.png'),
-            fit: BoxFit.fitWidth),
       ),
+
     );
   }
+
+  Widget AddPhoto(){
+    return Padding(
+  padding:EdgeInsets.only(left: 200),
+  child: IconButton(
+  onPressed: (){
+    getImage();
+    //widget.auth.getProfile();
+  },
+  icon: Icon(Icons.add_a_photo,size: 30,),
+  ),
+  );
+}
+//icon: Image.asset('assets/images/profile.png'),
 
   Widget TextUser(){
     return Container(
@@ -171,12 +207,61 @@ class _Profile_PageState extends State<Profile_Page> {
     );
   }
 
-
-
+  Widget SubmitButton(){
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 0),
+      child: Container(
+        width: 200,
+        height: 50,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(220, 41, 104, 1.0),
+                Color.fromRGBO(255, 118, 196, 1.0),
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(25.0),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.pink.withOpacity(0.2),
+                spreadRadius: 4,
+                blurRadius: 10,
+                offset: Offset(0, 3),
+              )
+            ]),
+        child: OutlinedButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+            ),
+          ),
+          onPressed: () {
+            uploadPic;
+          },
+          child: Text(
+            'Submit',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontFamily: "Roboto Slab",
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+              letterSpacing: 0.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   Widget SignoutButton(){
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 20),
+      padding: EdgeInsets.symmetric(vertical: 0),
       child: Container(
         width: 200,
         height: 50,
@@ -224,9 +309,8 @@ class _Profile_PageState extends State<Profile_Page> {
     );
   }
 
-
   Future<Null> editThread()async{
-      showDialog(context: context, builder: (context) => SimpleDialog(
+      showDialog(context: , builder: (context) => SimpleDialog(
           title: ListTile(
             leading:  Icon(Icons.account_box_outlined),
             title: Text('Edit Username'),
@@ -268,7 +352,7 @@ class _Profile_PageState extends State<Profile_Page> {
 
   Widget Forgotpassword() {
     return Padding(
-      padding: EdgeInsets.only(top: 70),
+      padding: EdgeInsets.only(top: 0),
       child: Container(
         width: 200,
         height: 50,
@@ -364,5 +448,31 @@ class _Profile_PageState extends State<Profile_Page> {
         ));
   }
 
-}
+  Future getImage () async{
 
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if(image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+
+  Future uploadPic(BuildContext context )async{
+
+    if (image == null) return;
+    final fileName = basename(image!.path);
+    final destination = 'files/$fileName';
+
+    try {
+      final ref = FirebaseStorage.instance.ref(destination).child('Profile/UserProfilePicture');//.putFile(file).onComplete;
+      await ref.putFile(image!);
+    } catch (e) {
+      print('error occured');
+    }
+
+  }
+}
