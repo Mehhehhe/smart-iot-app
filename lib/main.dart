9b101a378945618b126d7fd6cb10e043e6097d76
Mehhehhe <pakintada@gmail.com>
2022-11-28 18:24:36 +0700

    delete unused comment

--- lib/main.dart
+++ lib/main.dart
@@ -11,54 +11,14 @@ import 'package:smart_iot_app/pages/Login.dart';
 import 'package:aws_lambda_dart_runtime/aws_lambda_dart_runtime.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:smart_iot_app/services/PostRequest.dart';
+// import 'package:smart_iot_app/services/PostRequest.dart';
 
 import 'Theme/ThemeManager.dart';
 
-void main() async {
+void main(List list) async {
   // Check if app open properly
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MainPageWidgetObserver();
-  // ignore: prefer_function_declarations_over_variables
-  // Handler<AwsApiGatewayEvent> farmApiGateway = (context, event) async {
-  //   final resp = {
-  //     'message': 'Hello to ${context.requestId}',
-  //     'host': '${event.headers.host}',
-  //     'userAgent': '${event.headers.userAgent}',
-  //   };
-  //   final response = AwsApiGatewayResponse(
-  //     body: json.encode(resp),
-  //     isBase64Encoded: false,
-  //     statusCode: HttpStatus.ok,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   );
-  //   return response;
-  // };
-
-  // Runtime()
-  //   ..registerHandler<AwsApiGatewayEvent>("main.farm", farmApiGateway)
-  //   ..registerHandler("main.postRequest", PostRequest().postApiGateway)
-  //   ..invoke();
-
-  // Initialize AWS
-  // Remove this part.
-/*
-  try {
-    // Check if firebase already running in app. Instance: DEFAULT
-    Firebase.app('[DEFAULT]');
-  } catch (e) {
-    // If app just started...
-    await Firebase.initializeApp(
-      name: 'Smart IOT',
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyCB3LceCnDLpShGwKRSZ9NhW2Kg9txwv5U",
-          appId: "1:774636778498:android:cab8029c00fecc576f231b",
-          messagingSenderId: "774636778498",
-          projectId: "smartiotapp-8b124"),
-    );
-  }*/
   return runApp(ChangeNotifierProvider(
     create: (_) => ThemeNotifier(),
     child: SmartIOTApp(),
