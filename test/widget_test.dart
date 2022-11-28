abdb6fe278e658944a13a6069aef7d83a51bc1a7
Mehhehhe <pakintada@gmail.com>
2022-11-28 18:25:21 +0700

    was testing pipe

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,20 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_iot_app/features/widget_to_display_on_mainpage/cubit/farm_card_cubit.dart';
+import 'package:smart_iot_app/features/widget_to_display_on_mainpage/view/farm_card.dart';
 
-import 'package:smart_iot_app/main.dart';
+import 'package:smart_iot_app/main.dart' as smr_main;
+import 'package:smart_iot_app/modules/pipe.dart';
+import 'package:smart_iot_app/pages/MainPage.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Test piping was usable', () {
+    Iterable a = Iterable.empty();
+    add2(x) => x + 2;
+    sub2(x) => x - 2;
+    expect(a.pipe([add2, sub2])(0), equals(0));
   });
 }
