7c51c7e0b9750cba8b02722494c93b4bc4d52a41
Mehhehhe <pakintada@gmail.com>
2022-11-29 16:10:10 +0700

    v1.2.3AF1.T - remove username requirement

--- lib/features/widget_to_display_on_mainpage/view/farm_card.dart
+++ lib/features/widget_to_display_on_mainpage/view/farm_card.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_iot_app/features/widget_to_display_on_mainpage/cubit/farm_card_cubit.dart';
+import 'package:smart_iot_app/pages/MainPage.dart';
 
 import 'farm_card_view.dart';
 
 class farmCard extends StatelessWidget {
-  String username;
-  farmCard({Key? key, required this.username}) : super(key: key);
+  // String username;
+  int? farmIndex;
+  farmCard({Key? key}) : super(key: key);
+
+  farmCard.withIndex({Key? key, required this.farmIndex}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => FarmCardCubit(),
-      child: farmCardView(
-        username: username,
-      ),
+      child: MainPage(),
     );
   }
 }
