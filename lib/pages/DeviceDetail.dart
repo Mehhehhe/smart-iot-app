4bd87808c3cebc30508fb01edb3faeaf1cb01463
Mehhehhe <pakintada@gmail.com>
2022-11-30 21:13:09 +0700

    v1.2.3AF1.T - page for device detail

---
+++ lib/pages/DeviceDetail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeviceDetail extends StatelessWidget {
+  DeviceDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Container()));
+  }
+}
