4f5c0f7b209af3f53ca0809b55e844334c12cc31
Mehhehhe <pakintada@gmail.com>
2022-12-09 17:24:09 +0700

    v1.2.3AF1.5A - search bar widget

---
+++ lib/features/widget_to_display_on_mainpage/view/search_bar.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smart_iot_app/features/widget_to_display_on_mainpage/bloc/search_widget_bloc.dart';
+
+class SearchBar extends StatefulWidget {
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  final _textController = TextEditingController();
+  late SearchWidgetBloc _widgetBloc;
+
+  void _onCleared() {
+    _textController.clear();
+    _widgetBloc.add(const TextChanged(text: ""));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _widgetBloc = context.read<SearchWidgetBloc>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                const BoxShadow(
+                  blurRadius: 4,
+                  color: Color(0x33000000),
+                  offset: Offset(0, 2),
+                )
+              ],
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                  child: TextFormField(
+                    controller: _textController,
+                    autofocus: false,
+                    autocorrect: false,
+                    obscureText: false,
+                    onChanged: (value) {
+                      _widgetBloc.add(TextChanged(text: value));
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Search your device ...',
+                      hintStyle: Theme.of(context).textTheme.bodyText2,
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                      errorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                      focusedErrorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.send_sharp,
+                      ),
+                      suffixIcon: _textController!.text.isNotEmpty
+                          ? InkWell(
+                              onTap: _onCleared,
+                              child: const Icon(
+                                Icons.clear,
+                                color: Color(0xFF757575),
+                                size: 22,
+                              ),
+                            )
+                          : null,
+                    ),
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
