94272ed495b9fceb2f1ab4037e685d73f2c78c75
Mehhehhe <pakintada@gmail.com>
2022-12-12 21:08:28 +0700

    v1.3 - set initial to 1

--- lib/features/widget_to_display_on_mainpage/cubit/widget_in_flip_card_cubit.dart
+++ lib/features/widget_to_display_on_mainpage/cubit/widget_in_flip_card_cubit.dart
@@ -4,7 +4,7 @@ import 'package:equatable/equatable.dart';
 part 'widget_in_flip_card_state.dart';
 
 class BackOfCardCubit extends Cubit<BackOfCardInitial> {
-  BackOfCardCubit() : super(BackOfCardInitial(widgetIndex: 0));
+  BackOfCardCubit() : super(BackOfCardInitial(widgetIndex: 1));
 
   int currentIndex() => state.widgetIndex;
   void chooseIndex(int index) => emit(BackOfCardInitial(widgetIndex: index));
