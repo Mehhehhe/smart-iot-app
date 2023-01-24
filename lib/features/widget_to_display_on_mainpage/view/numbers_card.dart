0f66787776257d0ca8c12862cd1267e2fcac9a9a
Mehhehhe <pakintada@gmail.com>
2023-01-24 16:16:24 +0700

    v1.4F - change conds

--- lib/features/widget_to_display_on_mainpage/view/numbers_card.dart
+++ lib/features/widget_to_display_on_mainpage/view/numbers_card.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_iot_app/features/widget_to_display_on_mainpage/bloc/search_widget_bloc.dart';
 import 'package:smart_iot_app/features/widget_to_display_on_mainpage/bloc/user_data_stream_bloc.dart';
+import 'package:smart_iot_app/features/widget_to_display_on_mainpage/cubit/farm_card_cubit.dart';
+import 'package:smart_iot_app/features/widget_to_display_on_mainpage/cubit/live_data_cubit.dart';
 import 'package:smart_iot_app/model/SearchResult.dart';
 import 'package:smart_iot_app/pages/DeviceDetail.dart';
 import 'package:smart_iot_app/services/MQTTClientHandler.dart';
@@ -87,184 +89,208 @@ class _numberCardState extends State<numberCard> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return GridView.builder(
-      shrinkWrap: true,
-      itemCount: widget.devicesData == null ? 1 : widget.devicesData!.length,
-      itemBuilder: (context, index) {
-        return BlocBuilder<SearchWidgetBloc, SearchWidgetState>(
-          builder: (context, state) {
-            if (state is SearchWidgetSuccess && state.items.isNotEmpty) {
-              // if (state.items.isEmpty) {
-              //   return Container();
-              // }
-              var name = state.items[0].deviceName;
-              print("Build searched $name, from $data");
-              var currmap = Map<String, dynamic>.from(data
-                  .where((element) => element.keys.toString() == "($name)")
-                  .toList()[0]);
-              print("Query get : $currmap");
-              var currentValue = currmap[name]["Value"];
-              var details = getDetailOfDevice(name);
-              print("Target searched details: $details");
-              return Card(
-                child: InkWell(
-                  onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BlocProvider(
-                            create: (_) => UserDataStreamBloc(
-                                client: widget.existedCli,
-                                device: name,
-                                location: widget.whichFarm),
-                            child: DeviceDetail(
-                                detail: details,
-                                serial: name,
-                                location: widget.whichFarm,
-                                latestDatePlaceholder: [currmap])),
-                      )),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    child: Stack(
-                      children: [
-                        // gauge here!
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.2,
-                          child: SfRadialGauge(
-                            enableLoadingAnimation: true,
-                            title: GaugeTitle(text: name),
-                            axes: <RadialAxis>[
-                              RadialAxis(
-                                minimum: 0,
-                                maximum: 100,
-                                radiusFactor: 0.8,
-                                showLabels: false,
-                                showTicks: false,
-                                pointers: <GaugePointer>[
-                                  RangePointer(
-                                    value: double.parse(currentValue),
-                                    width: 18,
-                                    color: Colors.greenAccent,
-                                  ),
-                                ],
-                                annotations: [
-                                  GaugeAnnotation(
-                                      widget: Text(
-                                    double.parse(currentValue).toString(),
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ))
-                                ],
-                              )
+    return BlocBuilder<LiveDataCubit, LiveDataState>(
+      builder: (context, state) {
+        if (state.dataResponse.isNotEmpty) {
+          return GridView.builder(
+            shrinkWrap: true,
+            itemCount: (widget.devicesData == null || data.length == 0)
+                ? 1
+                : widget.devicesData!.length,
+            itemBuilder: (context, index) {
+              print("Building item: $data");
+              return BlocBuilder<SearchWidgetBloc, SearchWidgetState>(
+                builder: (context, state) {
+                  if (state is SearchWidgetSuccess && state.items.isNotEmpty) {
+                    // if (state.items.isEmpty) {
+                    //   return Container();
+                    // }
+                    var name = state.items[0].deviceName;
+                    print("Build searched $name, from $data");
+                    var currmap = Map<String, dynamic>.from(data
+                        .where(
+                            (element) => element.keys.toString() == "($name)")
+                        .toList()[0]);
+                    print("Query get : $currmap");
+                    var currentValue = currmap[name]["Value"];
+                    var details = getDetailOfDevice(name);
+                    print("Target searched details: $details");
+                    return Card(
+                      child: InkWell(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BlocProvider(
+                                  create: (_) => UserDataStreamBloc(
+                                      client: widget.existedCli,
+                                      device: name,
+                                      location: widget.whichFarm),
+                                  child: DeviceDetail(
+                                      detail: details,
+                                      serial: name,
+                                      location: widget.whichFarm,
+                                      latestDatePlaceholder: [currmap])),
+                            )),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.3,
+                          child: Stack(
+                            children: [
+                              // gauge here!
+                              Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                child: SfRadialGauge(
+                                  enableLoadingAnimation: true,
+                                  title: GaugeTitle(text: name),
+                                  axes: <RadialAxis>[
+                                    RadialAxis(
+                                      minimum: 0,
+                                      maximum: 100,
+                                      radiusFactor: 0.8,
+                                      showLabels: false,
+                                      showTicks: false,
+                                      pointers: <GaugePointer>[
+                                        RangePointer(
+                                          value: double.parse(currentValue),
+                                          width: 18,
+                                          color: Colors.greenAccent,
+                                        ),
+                                      ],
+                                      annotations: [
+                                        GaugeAnnotation(
+                                            widget: Text(
+                                          double.parse(currentValue).toString(),
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ))
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Center(
+                                  child: Padding(
+                                padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                                child: Switch(
+                                  value: currmap[name]!["State"],
+                                  onChanged: (value) {
+                                    widget.existedCli.publishToSetDeviceState(
+                                        widget.whichFarm, name, value);
+                                  },
+                                ),
+                              ))
                             ],
                           ),
                         ),
-                        Center(
-                            child: Padding(
-                          padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
-                          child: Switch(
-                            value: currmap[name]!["State"],
-                            onChanged: (value) {
-                              widget.existedCli.publishToSetDeviceState(
-                                  widget.whichFarm, name, value);
-                            },
-                          ),
-                        ))
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            } else if (state is SearchStateEmpty) {
-              var currmap = Map<String, Map<String, dynamic>>.from(data[index]);
-              var name = currmap.keys.first;
-              var currentValue = data[index][name]["Value"];
-              var details = getDetailOfDevice(name);
-              return Card(
-                child: InkWell(
-                  onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BlocProvider(
-                            create: (_) => UserDataStreamBloc(
-                                client: widget.existedCli,
-                                device: name,
-                                location: widget.whichFarm),
-                            child: DeviceDetail(
-                                detail: details,
-                                serial: name,
-                                location: widget.whichFarm,
-                                latestDatePlaceholder: [currmap])),
-                      )),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    child: Stack(
-                      children: [
-                        // gauge here!
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.2,
-                          child: SfRadialGauge(
-                            enableLoadingAnimation: true,
-                            title: GaugeTitle(text: name),
-                            axes: <RadialAxis>[
-                              RadialAxis(
-                                minimum: 0,
-                                maximum: 100,
-                                radiusFactor: 0.8,
-                                showLabels: false,
-                                showTicks: false,
-                                pointers: <GaugePointer>[
-                                  RangePointer(
-                                    value: double.parse(currentValue),
-                                    width: 18,
-                                    color: Colors.greenAccent,
-                                  ),
-                                ],
-                                annotations: [
-                                  GaugeAnnotation(
-                                      widget: Text(
-                                    double.parse(currentValue).toString(),
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ))
-                                ],
-                              )
+                      ),
+                    );
+                  } else if (state is SearchStateEmpty) {
+                    print("Enter empty cond, $data");
+
+                    var currmap =
+                        Map<String, Map<String, dynamic>>.from(data[index]);
+                    var name = currmap.keys.first;
+                    print(name);
+                    var currentValue = data[index][name]["Value"];
+                    var details = getDetailOfDevice(name);
+                    return Card(
+                      child: InkWell(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BlocProvider(
+                                  create: (_) => UserDataStreamBloc(
+                                      client: widget.existedCli,
+                                      device: name,
+                                      location: widget.whichFarm),
+                                  child: DeviceDetail(
+                                      detail: details,
+                                      serial: name,
+                                      location: widget.whichFarm,
+                                      latestDatePlaceholder: [currmap])),
+                            )),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.3,
+                          child: Stack(
+                            children: [
+                              // gauge here!
+                              Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                child: SfRadialGauge(
+                                  enableLoadingAnimation: true,
+                                  title: GaugeTitle(text: name),
+                                  axes: <RadialAxis>[
+                                    RadialAxis(
+                                      minimum: 0,
+                                      maximum: 100,
+                                      radiusFactor: 0.8,
+                                      showLabels: false,
+                                      showTicks: false,
+                                      pointers: <GaugePointer>[
+                                        RangePointer(
+                                          value: double.parse(currentValue),
+                                          width: 18,
+                                          color: Colors.greenAccent,
+                                        ),
+                                      ],
+                                      annotations: [
+                                        GaugeAnnotation(
+                                            widget: Text(
+                                          double.parse(currentValue).toString(),
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ))
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Center(
+                                  child: Padding(
+                                padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                                child: Switch(
+                                  value: currmap[name]!["State"],
+                                  onChanged: (value) {
+                                    widget.existedCli.publishToSetDeviceState(
+                                        widget.whichFarm, name, value);
+                                  },
+                                ),
+                              )),
                             ],
                           ),
                         ),
-                        Center(
-                            child: Padding(
-                          padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
-                          child: Switch(
-                            value: currmap[name]!["State"],
-                            onChanged: (value) {
-                              widget.existedCli.publishToSetDeviceState(
-                                  widget.whichFarm, name, value);
-                            },
-                          ),
-                        )),
-                      ],
+                      ),
+                    );
+                  } else if (state is SearchError) {
+                    print("State $state");
+                    return Container();
+                  }
+                  return Container(
+                    child: Center(
+                      child: Text(
+                        "No result. Please try again",
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ),
-                  ),
-                ),
+                  );
+                },
               );
-            } else if (state is SearchError) {
-              return Container();
-            }
-            return Container(
-              child: Center(
-                child: Text(
-                  "No result. Please try again",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            );
-          },
-        );
+            },
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: widget.devicesData == null ? 1 : 2),
+          );
+        } else if (data.isEmpty) {
+          return Container(
+            child: Center(
+              child: Text("No device"),
+            ),
+          );
+        }
+        return Container();
       },
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: widget.devicesData == null ? 1 : 2),
     );
   }
 }
