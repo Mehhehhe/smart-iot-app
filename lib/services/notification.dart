9d5a951cc56b60747e39c33384033c0b63568628
Mehhehhe <pakintada@gmail.com>
2023-02-28 20:14:38 +0700

    v1.4.1.1Re - notification, works with thresh

--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -1,29 +1,196 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smart_iot_app/db/threshold_settings.dart';
 
-class Threshold {
-  late SharedPreferences prefs;
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await initializeService();
+//   runApp(const MyApp());
+// }
 
-  init() async {
-    prefs = await SharedPreferences.getInstance();
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  /// OPTIONAL, using custom notification channel id
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'KarrIoT', // id
+    'KarrIoT SERVICE', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high, // importance must be at low or higher level
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  // if (Platform.isIOS) {
+  //   await flutterLocalNotificationsPlugin.initialize(
+  //     const InitializationSettings(
+  //       iOS: IOSInitializationSettings(),
+  //     ),
+  //   );
+  // }
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+
+      notificationChannelId: 'KarrIoT',
+      initialNotificationTitle: 'KarrIoT SERVICE',
+      initialNotificationContent: 'Working ... ',
+      foregroundServiceNotificationId: 888,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+
+      // this will be executed when app is in foreground in separated isolate
+      onForeground: onStart,
+
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+    ),
+  );
+
+  service.startService();
+}
+
+// to ensure this is executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+
+@pragma('vm:entry-point')
+Future<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.reload();
+  final log = preferences.getStringList('log') ?? <String>[];
+  log.add(DateTime.now().toIso8601String());
+  await preferences.setStringList('log', log);
+
+  return true;
+}
+
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  // Only available for flutter 3.0.0 and later
+  DartPluginRegistrant.ensureInitialized();
+
+  // For flutter prior to version 3.0.0
+  // We have to register the plugin manually
+
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  // await preferences.setString("hello", "world");
+
+  /// OPTIONAL when use custom notification
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
   }
 
-  void save(Map data) {
-    for (var d in data.entries) {
-      switch (d.value.runtimeType) {
-        case double:
-          prefs.setDouble(d.key, d.value);
-          break;
-        case bool:
-          prefs.setBool(d.key, d.value);
-          break;
-        case String:
-          prefs.setString(d.key, d.value);
-          break;
-        default:
-          throw ("Unknown Type. Please check the value.");
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  // bring to foreground
+  // Timer.periodic(const Duration(seconds: 1), (timer) async {
+  //   if (service is AndroidServiceInstance) {
+  //     if (await service.isForegroundService()) {
+  //       /// OPTIONAL for use custom notification
+  //       /// the notification id must be equals with AndroidConfiguration when you call configure() method.
+  //       flutterLocalNotificationsPlugin.show(
+  //         888,
+  //         'COOL SERVICE',
+  //         'Awesome ${DateTime.now()}',
+  //         const NotificationDetails(
+  //           android: AndroidNotificationDetails(
+  //             'my_foreground',
+  //             'MY FOREGROUND SERVICE',
+  //             icon: 'ic_bg_service_small',
+  //             ongoing: true,
+  //           ),
+  //         ),
+  //       );
+  //     }
+  //   }
+  // });
+
+  // Fetching `threshold` sql, display notification
+  service.on('threshDiff').listen((event) async {
+    // Compare to thresh
+    final threshdb = ThresholdDatabase.instance;
+    String currId = event!["encryptedKey"];
+    print("[Background] $currId , ${event["name"]}");
+    List thAll = await threshdb.getAllAvailableThresh();
+    print("[AllTHDB] $thAll");
+    int activationTh = await threshdb.getThresh(currId);
+
+    if (service is AndroidServiceInstance) {
+      if (await service.isForegroundService() &&
+          int.parse(event["value"]) > activationTh) {
+        flutterLocalNotificationsPlugin.show(
+          888,
+          'KarrIoT',
+          "${event["name"]} exceeds the set threshold of $activationTh with value ${event["value"]}",
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'KarrIoT',
+              'KarrIoT SERVICE',
+              icon: 'ic_bg_service_small',
+              ongoing: true,
+              importance: Importance.high,
+              priority: Priority.high,
+            ),
+          ),
+        );
       }
     }
-  }
+  });
+  // Timer.periodic(const Duration(seconds: 10), (timer) async {
+  //   final threshdb = ThresholdDatabase.instance;
+  //   List activationTh = await threshdb.getAllAvailableThresh();
 
-  getThresh(String item) => prefs.get(item);
+  //   for(var i = 0; i < activationTh.length; i++){
+  //     var currId = activationTh[i]["_id"];
+  //     var currTh = activationTh[i]["_threshVal"];
+  //     bool diff = false;
+  //     // Wait for incoming values,
+
+  //     if (service is AndroidServiceInstance) {
+  //       if (await service.isForegroundService()) {
+  //       flutterLocalNotificationsPlugin.show(
+  //           888, 'KarrIoT', ,);
+  //       }
+  //     }
+  //   }
+
+  // });
 }
