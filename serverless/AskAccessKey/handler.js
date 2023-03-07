72ba07efccb6c56e81abfb0c69adb7b2dd4c4b33
Mehhehhe <pakintada@gmail.com>
2023-03-07 12:41:35 +0700

    was testing

---
+++ serverless/AskAccessKey/handler.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const FarmTable = process.env.FARM_TABLE;
+const FarmUserTable = process.env.FARM_USER_TABLE;
+const FarmDeviceTable = process.env.FARM_DEVICE_TABLE;
+
+AWS.
+
+module.exports.retreiveAccessKey = (event, context, callback) => {
+
+};
