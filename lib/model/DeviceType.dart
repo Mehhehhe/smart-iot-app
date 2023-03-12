59ba1ee859fe5a2939a8b42ec78c5a294a90a1a8
Mehhehhe <pakintada@gmail.com>
2023-03-12 18:35:37 +0700

    v1.4.1.4C - change props

--- lib/model/DeviceType.dart
+++ lib/model/DeviceType.dart
@@ -1,21 +1,34 @@
-enum DEVICEVAL_TYPE {
-  TEMPERATURE_IN_AIR,
-  HUMIDITY_IN_AIR,
-  NPK,
-  HUMIDITY_IN_SOIL
+// ignore: file_names
+enum DeviceValType {
+  temperature,
+  humidity,
+  npk,
+  // HUMIDITY_IN_SOIL
 }
 
 class DeviceType {
   final properties = {
-    'temperature': {'min': 0.0, 'max': 100.0, 'unit': 'celsius'},
-    'humidity': {'min': 0.0, 'max': 100.0, 'unit': 'percent'},
-    'npk': {}
+    'temperature': {
+      'min': 0.0,
+      'max': 100.0,
+      'unit': 'celsius',
+    },
+    'humidity': {
+      'min': 0.0,
+      'max': 100.0,
+      'unit': 'percent',
+    },
+    'npk': {
+      'min': 0.0,
+      'max': 2000.0,
+      'unit': 'mg/kg',
+    },
+    'light': {
+      'min': 0.0,
+      'max': 100000.0,
+      'unit': 'lx',
+    },
   };
-}
 
-class Device {
-  final String device_type;
-  const Device._({required this.device_type});
-  const Device.temperature(dynamic data, [String device_type = "temperature"])
-      : this._(device_type: device_type);
+  getProps(String type) => properties[type];
 }
