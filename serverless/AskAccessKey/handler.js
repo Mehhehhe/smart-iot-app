9191c1d96a094ebd5c8439c0ee9c7e89d8416bae
Mehhehhe <pakintada@gmail.com>
2023-03-13 14:58:01 +0700

    -

--- serverless/AskAccessKey/handler.js
+++ serverless/AskAccessKey/handler.js
@@ -7,8 +7,6 @@ const FarmTable = process.env.FARM_TABLE;
 const FarmUserTable = process.env.FARM_USER_TABLE;
 const FarmDeviceTable = process.env.FARM_DEVICE_TABLE;
 
-AWS.
-
 module.exports.retreiveAccessKey = (event, context, callback) => {
 
 };
