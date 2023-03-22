95a1a168883909d32697d3784287f8fbd0f7ee4c
Mehhehhe <pakintada@gmail.com>
2023-03-22 15:44:41 +0700

    v1.4.1.6A - add google login

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,23 +1,32 @@
 import 'dart:async';
 
+// import 'package:amazon_cognito_identity_dart_2/cognito.dart';
+import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_user_agentx/flutter_user_agent.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:smart_iot_app/features/widget_to_display_on_mainpage/bloc/user_data_stream_bloc.dart';
+// import 'package:smart_iot_app/features/widget_to_display_on_mainpage/bloc/user_data_stream_bloc.dart';
 import 'package:smart_iot_app/features/widget_to_display_on_mainpage/view/farm_card.dart';
-import 'package:smart_iot_app/pages/MainPage.dart';
-import 'package:smart_iot_app/services/MQTTClientHandler.dart';
+// import 'package:smart_iot_app/pages/MainPage.dart';
+// import 'package:smart_iot_app/services/MQTTClientHandler.dart';
 import 'package:smart_iot_app/services/authentication.dart';
 
 // Amplify Flutter Packages
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 // Generated in previous step
 import '../amplifyconfiguration.dart';
 import 'onboard.dart';
+
+import '../secrets/secrets.dart';
 
 // Initialize AWS Auth in authentication page instead.
 Future<void> _configureAmplify() async {
@@ -34,7 +43,8 @@ Future<void> _configureAmplify() async {
 class LogIn extends StatefulWidget {
   //const LogIn({Key? key, required this.auth, required this.loginCallback})
   //    : super(key: key);
-  const LogIn({Key? key}) : super(key: key);
+  final InAppBrowser browser = InAppBrowser();
+  LogIn({Key? key}) : super(key: key);
 
   //final BaseAuth auth;
   //final VoidCallback loginCallback;
@@ -68,65 +78,77 @@ class _LogIn extends State<LogIn> {
   }
 
   Widget _awsAuth() {
+    // return Scaffold(
+    //   appBar: AppBar(title: Text("Test Web Auth")),
+    //   body: Column(children: [
+    //     ElevatedButton(
+    //       onPressed: () => hostedUIWebView(),
+    //       child: const Text("Sign in"),
+    //     ),
+    //   ]),
+    // );
     return Authenticator(
-        authenticatorBuilder: (p0, p1) {
-          const padding =
-              EdgeInsets.only(left: 16, right: 16, top: 48, bottom: 16);
-          switch (p1.currentStep) {
-            case AuthenticatorStep.loading:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.onboarding:
-              break;
-            case AuthenticatorStep.signUp:
-              return _signUpForm(padding, p1);
-              break;
-            case AuthenticatorStep.signIn:
-              return _signInForm(padding, p1);
-              break;
-            case AuthenticatorStep.confirmSignUp:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.confirmSignInCustomAuth:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.confirmSignInMfa:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.confirmSignInNewPassword:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.resetPassword:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.confirmResetPassword:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.verifyUser:
-              // TODO: Handle this case.
-              break;
-            case AuthenticatorStep.confirmVerifyUser:
-              // TODO: Handle this case.
-              break;
-          }
-        },
-        // initialStep: AuthenticatorStep.signUp,
-        child: MaterialApp(
-          builder: Authenticator.builder(),
-          debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            body: FutureBuilder(
-              future: _isOnboardingCompleted(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData && snapshot.data == true) {
-                  return farmCard();
-                } else {
-                  return OnboardingPage();
-                }
-              },
-            ),
-          ),
-        ));
+      authenticatorBuilder: (p0, p1) {
+        const padding =
+            EdgeInsets.only(left: 16, right: 16, top: 48, bottom: 16);
+        switch (p1.currentStep) {
+          case AuthenticatorStep.loading:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.onboarding:
+            break;
+          case AuthenticatorStep.signUp:
+            return _signUpForm(padding, p1);
+            break;
+          case AuthenticatorStep.signIn:
+            return _signInForm(padding, p1);
+            break;
+          case AuthenticatorStep.confirmSignUp:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.confirmSignInCustomAuth:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.confirmSignInMfa:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.confirmSignInNewPassword:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.resetPassword:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.confirmResetPassword:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.verifyUser:
+            // TODO: Handle this case.
+            break;
+          case AuthenticatorStep.confirmVerifyUser:
+            // TODO: Handle this case.
+            break;
+        }
+      },
+      // initialStep: AuthenticatorStep.signUp,
+      child: mainApp(),
+    );
+  }
+
+  Widget mainApp() {
+    return MaterialApp(
+      builder: Authenticator.builder(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: FutureBuilder(
+          future: _isOnboardingCompleted(),
+          builder: (context, snapshot) {
+            return snapshot.hasData && snapshot.data == true
+                ? farmCard()
+                : OnboardingPage();
+          },
+        ),
+      ),
+    );
   }
 
   Widget _signUpForm(EdgeInsets padding, AuthenticatorState state) {
@@ -171,6 +193,7 @@ class _LogIn extends State<LogIn> {
     );
   }
 
+  // ignore: long-method
   Widget _signInForm(EdgeInsets padding, AuthenticatorState state) {
     return Scaffold(
       body: Padding(
@@ -181,28 +204,26 @@ class _LogIn extends State<LogIn> {
               Stack(
                 children: [
                   Container(
-                    height: 450,
+                    height: 700,
                     padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
                     margin: const EdgeInsets.only(top: 70),
-                    // decoration: BoxDecoration(
-                    //     color: Colors.amber,
-                    //     borderRadius: BorderRadius.circular(25),
-                    //     boxShadow: const [
-                    //       BoxShadow(
-                    //         blurRadius: 4,
-                    //         color: Color(0x33000000),
-                    //         offset: Offset(0, 5),
-                    //       )
-                    //     ]),
                     child: Column(
                       children: [
-                        SignInForm.custom(fields: [
-                          SignInFormField.username(),
-                          SignInFormField.password()
-                        ]),
+                        SignInForm(includeDefaultSocialProviders: true),
+                        // ElevatedButton(
+                        //   child: Text("Test login"),
+                        //   onPressed: () => Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //       builder: (context) => getGoogleWebView(),
+                        //     ),
+                        //   ),
+                        // ),
                         const Divider(
                           color: Colors.black,
-                          height: 30,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -212,14 +233,15 @@ class _LogIn extends State<LogIn> {
                               style: TextStyle(fontSize: 18),
                             ),
                             TextButton(
-                                onPressed: () =>
-                                    state.changeStep(AuthenticatorStep.signUp),
-                                child: const Text(
-                                  "Sign up",
-                                  style: TextStyle(fontSize: 18),
-                                ))
+                              onPressed: () =>
+                                  state.changeStep(AuthenticatorStep.signUp),
+                              child: const Text(
+                                "Sign up",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
                           ],
-                        )
+                        ),
                       ],
                     ),
                   ),
@@ -229,30 +251,6 @@ class _LogIn extends State<LogIn> {
                     ),
                   ),
                   // Center(
-                  //   child: CircleAvatar(
-                  //     radius: 80,
-                  //     backgroundColor: Colors.black87,
-                  //     child: Container(
-                  //       height: 110,
-                  //       width: 110,
-                  //       decoration: BoxDecoration(
-                  //         shape: BoxShape.circle,
-                  //         color: Colors.white,
-                  //         boxShadow: [
-                  //           BoxShadow(
-                  //             color: Colors.black26,
-                  //             offset: Offset(0, 2),
-                  //             blurRadius: 6,
-                  //           ),
-                  //         ],
-                  //       ),
-                  //       child: Image.network(
-                  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBwvh_i0UMh_LTBJ17Ct47fqBSwLVy142SgYa7QpZxbIuV_6Mn_UKLvObmWvzvJw0r92c&usqp=CAU",
-                  //         fit: BoxFit.cover,
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
                 ],
               ),
             ],
@@ -264,6 +262,89 @@ class _LogIn extends State<LogIn> {
 
   _isOnboardingCompleted() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+
     return prefs.getBool('onboarding') ?? false;
+  }
+
+  _googleWebViewController(uri) {
+    print("[GoogleWebView] $uri");
+
+    return WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent("random")
+      ..setBackgroundColor(Colors.white)
+      ..setNavigationDelegate(NavigationDelegate(
+        onWebResourceError: (error) => print(
+            "[WebError] ${error.errorCode} :${error.description}, ${error.errorType}"),
+        onNavigationRequest: (request) async {
+          Map query = Uri.splitQueryString(request.url);
+          print("[RequestWebView] ${query}");
+          if (query.containsKey("karriot://?code")) {
+            String code = query["karriot://?code"];
+            CognitoUser user = await Auth().signInWithGoogle(code);
+            if (user.username != null) {
+              // ignore: use_build_context_synchronously
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FutureBuilder(
+                    future: _isOnboardingCompleted(),
+                    builder: (context, snapshot) {
+                      return snapshot.hasData && snapshot.data == true
+                          ? farmCard()
+                          : OnboardingPage();
+                    },
+                  ),
+                ),
+              );
+            }
+
+            return NavigationDecision.prevent;
+          }
+
+          return NavigationDecision.navigate;
+        },
+      ))
+      ..loadRequest(uri);
+  }
+
+  Widget getGoogleWebView() {
+    var url = Secret().getAuthUrl();
+    print(url);
+
+    return WebViewWidget(controller: _googleWebViewController(url));
+  }
+
+  Widget hostedUIWebView() {
+    var loginUrl = Secret().loginUrl();
+    print(loginUrl);
+    WebViewController wv = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent("random")
+      ..setBackgroundColor(Colors.white)
+      ..setNavigationDelegate(NavigationDelegate(
+        // onProgress: ,
+        onWebResourceError: (error) =>
+            print("[WebHostError] ${error.errorCode}:${error.description}"),
+        // onPageStarted: ,
+        // onPageFinished: ,
+        onNavigationRequest: (request) async {
+          print("[HostUI] ${request.url}");
+          Map query = Uri.splitQueryString(request.url);
+          if (query.containsKey("karriot://?code")) {
+            String code = query["karriot://?code"];
+            // post to token endpoint method
+
+            return NavigationDecision.prevent;
+          }
+
+          return NavigationDecision.navigate;
+        },
+      ))
+      ..loadRequest(loginUrl);
+
+    return WebViewWidget(
+      controller: wv,
+    );
   }
 }
