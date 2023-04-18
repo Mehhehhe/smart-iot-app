b49844828e9f6be69b06fc8b3b373a426f5fdb32
Mehhehhe <pakintada@gmail.com>
2023-04-18 15:00:21 +0700

    `v1.4.1.6YTS` - fix farm selector

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -1,24 +1,13 @@
 // ignore: file_names
-import 'dart:convert';
 
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:smart_iot_app/features/widget_to_display_on_mainpage/bloc/farm_card_re_bloc.dart';
-import 'package:smart_iot_app/features/widget_to_display_on_mainpage/bloc/search_widget_bloc.dart';
-// import 'package:smart_iot_app/features/widget_to_display_on_mainpage/cubit/farm_card_cubit.dart';
-// import 'package:smart_iot_app/features/widget_to_display_on_mainpage/cubit/screen_index_change_cubit.dart';
-import 'package:smart_iot_app/features/widget_to_display_on_mainpage/view/farm_card.dart';
 import 'package:smart_iot_app/features/widget_to_display_on_mainpage/view/farm_card_view.dart';
 import 'package:smart_iot_app/features/widget_to_display_on_mainpage/view/farm_editor.dart';
-import 'package:smart_iot_app/model/SearchResult.dart';
-import 'package:smart_iot_app/pages/Login.dart';
-import 'package:smart_iot_app/pages/settings.dart';
 import 'package:smart_iot_app/services/MQTTClientHandler.dart';
 import 'package:smart_iot_app/services/lambdaCaller.dart';
-
-import '../features/widget_to_display_on_mainpage/view/history_log.dart';
 
 class MainPage extends StatefulWidget {
   MQTTClientWrapper cli;
@@ -77,6 +66,7 @@ class _MainPageState extends State<MainPage> {
     setState(() {
       farmInd = value;
     });
+    print("[AfterSelected] $farmInd");
   }
 
   @override
@@ -129,112 +119,108 @@ class _MainPageState extends State<MainPage> {
               ),
               child: BlocConsumer<FarmCardReBloc, FarmCardReState>(
                 bloc: context.read<FarmCardReBloc>(),
-                listener: (context, state) {},
+                listener: (context, state) {
+                  if (state.farms.isNotEmpty) {
+                    ownedFarm = state.farms;
+                  }
+                },
                 builder: (context, state) {
                   return farmCardView(
                     username: accountName,
-                    overrideFarmIndex:
-                        state.farmIndex == farmInd ? state.farmIndex : farmInd,
+                    overrideFarmIndex: farmInd,
                     cli: widget.cli,
                   );
                 },
               ),
             ),
             backgroundColor: Colors.transparent,
-            // drawer: Drawer(
-            //   elevation: 5.0,
-            //   child: Material(
-            //     child: Padding(
-            //       padding: const EdgeInsets.all(0),
-            //       child: ListView(
-            //         children: [
-            //           Builder(
-            //             builder: (context) {
-            //               accountEmail = account["id"];
-            //               accountName = account["name"];
+            drawer: Drawer(
+              elevation: 5.0,
+              child: Material(
+                child: Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: ListView(
+                    children: [
+                      // Builder(
+                      //   builder: (context) {
+                      //     accountEmail = account["id"];
+                      //     accountName = account["name"];
 
-            //               return UserAccountsDrawerHeader(
-            //                 accountName: Text(
-            //                   accountName,
-            //                   style: Theme.of(context).textTheme.headlineSmall,
-            //                 ),
-            //                 accountEmail: Text(
-            //                   accountEmail,
-            //                   style: const TextStyle(
-            //                     color: Colors.black,
-            //                     fontWeight: FontWeight.bold,
-            //                   ),
-            //                 ),
-            //                 decoration: const BoxDecoration(
-            //                   color: Colors.green,
-            //                 ),
-            //               );
-            //             },
-            //           ),
-            //           // UserAccountsDrawerHeader(
-            //           //     accountName: Text(accountName ?? "Name",
-            //           //         style: Theme.of(context).textTheme.headline5),
-            //           //     accountEmail: Text(accountEmail ?? "Email",
-            //           //         style: Theme.of(context).textTheme.headline6)),
-            //           ListTile(
-            //             title: const Text(
-            //               "เปลี่ยนฟาร์ม",
-            //               style: TextStyle(fontSize: 22),
-            //             ),
-            //             isThreeLine: true,
-            //             hoverColor: Colors.white70,
-            //             subtitle: const Text(""),
-            //             onTap: () async {
-            //               // _displayFarmEditor(context, data);
-            //               await Navigator.push(
-            //                 context,
-            //                 MaterialPageRoute(
-            //                   builder: (context) => FarmEditor(farm: ownedFarm),
-            //                 ),
-            //               ).then((value) {
-            //                 onIndexSelection(value);
-            //                 context
-            //                     .read<FarmCardReBloc>()
-            //                     .chooseIndex(index, ownedFarm);
-            //               });
-            //             },
-            //             onLongPress: () => const Dialog(
-            //               child: Text(
-            //                 "เลือกฟาร์มของคุณ กด 1 ครั้งเพื่อนำทางไปยังหน้าต่างเลือกฟาร์ม โดยจะแสดงผลเป็นรายชื่อฟาร์มทั้งหมด",
-            //               ),
-            //             ),
-            //           ),
+                      //     return UserAccountsDrawerHeader(
+                      //       accountName: Text(
+                      //         accountName,
+                      //         style: Theme.of(context).textTheme.headlineSmall,
+                      //       ),
+                      //       accountEmail: Text(
+                      //         accountEmail,
+                      //         style: const TextStyle(
+                      //           color: Colors.black,
+                      //           fontWeight: FontWeight.bold,
+                      //         ),
+                      //       ),
+                      //       decoration: const BoxDecoration(
+                      //         color: Colors.green,
+                      //       ),
+                      //     );
+                      //   },
+                      // ),
+                      // UserAccountsDrawerHeader(
+                      //     accountName: Text(accountName ?? "Name",
+                      //         style: Theme.of(context).textTheme.headline5),
+                      //     accountEmail: Text(accountEmail ?? "Email",
+                      //         style: Theme.of(context).textTheme.headline6)),
+                      ListTile(
+                        title: const Text(
+                          "Change farm",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        isThreeLine: true,
+                        hoverColor: Colors.white70,
+                        subtitle: const Text(""),
+                        onTap: () async {
+                          // _displayFarmEditor(context, data);
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => FarmEditor(farm: ownedFarm),
+                            ),
+                          ).then((value) {
+                            // context.read<FarmCardReBloc>().chooseIndex(index);
+                            onIndexSelection(value);
+                          });
+                        },
+                      ),
 
-            //           ListTile(
-            //             title: const Text(
-            //               "Profile",
-            //               style: TextStyle(fontSize: 22),
-            //             ),
-            //             isThreeLine: true,
-            //             subtitle: const Text("ดูหน้าโปรไฟล์และแก้ไขข้อมูล"),
-            //             hoverColor: Colors.white70,
-            //             onTap: () {},
-            //           ),
+                      // ListTile(
+                      //   title: const Text(
+                      //     "Profile",
+                      //     style: TextStyle(fontSize: 22),
+                      //   ),
+                      //   isThreeLine: true,
+                      //   subtitle: const Text("ดูหน้าโปรไฟล์และแก้ไขข้อมูล"),
+                      //   hoverColor: Colors.white70,
+                      //   onTap: () {},
+                      // ),
 
-            //           ListTile(
-            //             title: const Text(
-            //               "Setting",
-            //               style: TextStyle(fontSize: 22),
-            //             ),
-            //             isThreeLine: true,
-            //             subtitle: const Text("การตั้งค่าภายในแอพและอื่นๆ"),
-            //             hoverColor: Colors.white70,
-            //             onTap: () => Navigator.of(context).push(
-            //               MaterialPageRoute(
-            //                 builder: (context) => SettingPage(),
-            //               ),
-            //             ),
-            //           ),
-            //         ],
-            //       ),
-            //     ),
-            //   ),
-            // ),
+                      // ListTile(
+                      //   title: const Text(
+                      //     "Setting",
+                      //     style: TextStyle(fontSize: 22),
+                      //   ),
+                      //   isThreeLine: true,
+                      //   subtitle: const Text("การตั้งค่าภายในแอพและอื่นๆ"),
+                      //   hoverColor: Colors.white70,
+                      //   onTap: () => Navigator.of(context).push(
+                      //     MaterialPageRoute(
+                      //       builder: (context) => SettingPage(),
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       ),
