044bc5566358a51ff3391ced16fc9b8d2fe33937
Mehhehhe <pakintada@gmail.com>
2023-04-23 21:34:39 +0700

    Disable debug

--- lib/features/mainpage_widget_observer.dart
+++ lib/features/mainpage_widget_observer.dart
@@ -4,6 +4,6 @@ class MainPageWidgetObserver extends BlocObserver {
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    print("${bloc.runtimeType} $change");
+    // print("${bloc.runtimeType} $change");
   }
 }
